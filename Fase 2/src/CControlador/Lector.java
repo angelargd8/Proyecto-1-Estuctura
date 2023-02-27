8cde52f20be3a4c295dbd6a4bac677f249e390e6
JosueSay <106031855+JosueSay@users.noreply.github.com>
2023-02-27 11:18:59 -0600

    inicio_controlador

---
+++ Fase 2/src/CControlador/Lector.java
@@ -0,0 +1,20 @@
+package CControlador;
+
+public class Lector {
+
+	public void menu() {
+		
+		System.out.println("=======================");
+		System.out.println("||Traductor LISP-JAVA||");
+		System.out.println("=======================");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
