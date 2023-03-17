678ce1e2ad4b902beed5a3a0b7d1347c7fc78d67
angelargd8 <angelargd8@outlook.com>
2023-03-17 00:52:13 -0600

    ya me duermo xd

---
+++ Fase 2/Fase2/src/CModelos/Funciones.java
@@ -0,0 +1,18 @@
+package CModelos;
+
+import java.util.HashMap;
+
+public class Funciones {
+    HashMap hashMap;
+    String nombreFuncioString;
+    
+
+    public Funciones(){
+        hashMap = new HashMap();
+    }
+
+    //constructor si no ando mal
+
+
+
+}
