f3fe2281b58068e685a730fe1014306ff5b87cff
angelargd8 <angelargd8@outlook.com>
2023-03-17 21:14:02 -0600

    xd

--- Fase 2/Fase2/src/CVista/Main.java
+++ Fase 2/Fase2/src/CVista/Main.java
@@ -1,3 +1,12 @@
+ /** 
+     * Proyecto 1 Estructura de datos
+     * Autores: 
+     *          - Alejandro Ortega #18248
+     *          - Angela García #22869
+     *          - Josué Say #22801
+     * 
+     */
+
 package CVista;
 
 public class Main {
